528ad2ec77158d6442cacebc9faebddb8035bef1
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2020-02-08 15:09:45 +0700

    Delete comment on register.js file

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,17 +1,17 @@
-// if ( !( 'serviceWorker' ) in navigator ) {
-//     console.error( "Service worker tidak didukung browser ini." );
-// } else {
-//     registerServiceWorker();
-// }
+if ( !( 'serviceWorker' ) in navigator ) {
+    console.error( "Service worker tidak didukung browser ini." );
+} else {
+    registerServiceWorker();
+}
 
-// // Registrasi service worker
-// async function registerServiceWorker() {
-//     try {
-//         const registration = await navigator.serviceWorker.register( './sw.js' );
-//         console.log( 'Registrasi service worker berhasil.' );
-//         return registration;
-//     }
-//     catch ( err ) {
-//         console.error( 'Registrasi service worker gagal.', err );
-//     }
-// }
+// Registrasi service worker
+async function registerServiceWorker() {
+    try {
+        const registration = await navigator.serviceWorker.register( './sw.js' );
+        console.log( 'Registrasi service worker berhasil.' );
+        return registration;
+    }
+    catch ( err ) {
+        console.error( 'Registrasi service worker gagal.', err );
+    }
+}
